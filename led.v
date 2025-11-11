fa237543d5efd278ec58da1524657be91ca67d42
Thulasi Thavarajah <thulasi49thava@gmail.com>
2025-11-11 08:51:09 -0500

    Update led.v

--- led.v
+++ led.v
@@ -1,3 +1,4 @@
+
 module led (CLOCK_50, LEDR, t, index);
     input CLOCK_50;
     output reg [3:0] LEDR;
@@ -10,16 +11,17 @@ module led (CLOCK_50, LEDR, t, index);
     wire [71:0] col4 = {1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b1,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b1,1'b0,1'b0,1'b0,1'b0,1'b0,1'b1,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b1,1'b1,1'b0,1'b0,1'b0,1'b0,1'b1,1'b1,1'b1,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0,1'b0};
  
     third_counter tc (CLOCK_50, t);
-    always @ (posedge t)
+    always @ (posedge CLOCK_50)
     begin
+        if(t)
         LEDR[3] <= col1[index];
         LEDR[2] <= col2[index];
         LEDR[1] <= col3[index];
         LEDR[0] <= col4[index];
         if (index == 7'd71)
-            index = 7'd0;
+            index <= 7'd0;
         else
-            index = index + 1'b1;
+            index <= index + 1'b1;
     end
 endmodule
 
@@ -27,17 +29,19 @@ module third_counter (CLOCK_50, t);
     input CLOCK_50;
     reg [24:0] little = 25'd0;
     output reg t = 1'b0;
+    
     always @ (posedge CLOCK_50)
     begin
         if (little == 25'd16666666)
             begin
-                little <= 0;
+                little <=25'd0;
                 t <= 1'b1;
             end
         else
             begin
-                t <= 0;
+                t <= 1'b0;
                 little <= little + 1;
             end
     end
+    
 endmodule
